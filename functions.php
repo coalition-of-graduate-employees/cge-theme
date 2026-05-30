8fc60672fd439ddde6d04095d39c8f3c26832ac8
Andrew Scherer <ascherer97@gmail.com>
2026-05-30 14:31:28 -0700

    more robust hiding of title in editor and hide unused templates

--- functions.php
+++ functions.php
@@ -22,11 +22,27 @@ function cge_theme_enqueue_assets() {
 }
 add_action('wp_enqueue_scripts', 'cge_theme_enqueue_assets');
 
-function cge_theme_hide_front_page_title_in_editor() {
+function cge_theme_is_editing_front_page() {
     $front_page_id = (int) get_option('page_on_front');
-    $current_id = (int) get_the_ID();
 
-    if ($front_page_id && $current_id === $front_page_id) {
+    if (!$front_page_id) {
+        return false;
+    }
+
+    $post_id = 0;
+    if (isset($_GET['post'])) {
+        $post_id = (int) $_GET['post'];
+    } elseif (isset($_POST['post_ID'])) {
+        $post_id = (int) $_POST['post_ID'];
+    } else {
+        $post_id = (int) get_the_ID();
+    }
+
+    return $post_id === $front_page_id;
+}
+
+function cge_theme_hide_front_page_title_in_editor() {
+    if (cge_theme_is_editing_front_page()) {
         $css = '.editor-styles-wrapper .edit-post-visual-editor__post-title-wrapper{display:none;}';
         wp_add_inline_style('wp-edit-blocks', $css);
     }
@@ -41,3 +57,21 @@ function cge_theme_hide_front_page_title_block($block_content, $block) {
     return $block_content;
 }
 add_filter('render_block', 'cge_theme_hide_front_page_title_block', 10, 2);
+
+function cge_theme_filter_block_templates($block_templates, $query, $template_type) {
+    if ($template_type !== 'wp_template') {
+        return $block_templates;
+    }
+
+    $hidden_slugs = array(
+        'archive',
+        'home',
+        'search',
+        'page-no-title'
+    );
+
+    return array_values(array_filter($block_templates, function ($template) use ($hidden_slugs) {
+        return !in_array($template->slug, $hidden_slugs, true);
+    }));
+}
+add_filter('get_block_templates', 'cge_theme_filter_block_templates', 10, 3);
