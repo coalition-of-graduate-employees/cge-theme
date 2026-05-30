61caeb0da88d384509a5f4ae66d64d8e6ff28d3a
Andrew Scherer <ascherer97@gmail.com>
2026-05-30 15:13:50 -0700

    add fact block for info about our org

---
+++ patterns/fact-stat.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Title: Fact Stat
+ * Slug: cge-theme/fact-stat
+ * Categories: text
+ * Description: A fact stat with a heading, separator bar, and subheading.
+ */
+?>
+<!-- wp:group {"layout":{"type":"constrained"},"style":{"spacing":{"blockGap":"0.75rem"}},"lock":{"move":false,"remove":false}} -->
+<div class="wp-block-group" data-lock="{"move":false,"remove":false}">
+  <!-- wp:heading {"level":2,"placeholder":"Heading","lock":{"move":false,"remove":false}} -->
+  <h2 data-lock="{"move":false,"remove":false}"></h2>
+  <!-- /wp:heading -->
+
+  <!-- wp:separator {"className":"is-style-wide","style":{"color":{"background":"#3e173d"}},"lock":{"move":false,"remove":false}} /-->
+
+  <!-- wp:paragraph {"fontSize":"sm","placeholder":"Subheading","lock":{"move":false,"remove":false}} -->
+  <p class="has-sm-font-size" data-lock="{"move":false,"remove":false}"></p>
+  <!-- /wp:paragraph -->
+</div>
+<!-- /wp:group -->
