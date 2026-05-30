f1116eda817bf7b066886242d36f0d6f91912d50
Andrew Scherer <ascherer97@gmail.com>
2026-05-30 15:34:56 -0700

    more style updates

--- patterns/fact-stat.php
+++ patterns/fact-stat.php
@@ -8,24 +8,28 @@
 ?>
 <!-- wp:group {"layout":{"type":"constrained"},"style":{"spacing":{"blockGap":"0.75rem"}},"lock":{"move":false,"remove":false}} -->
 <div class="wp-block-group" style="text-align:center">
-  <!-- wp:heading {"level":2,"placeholder":"Heading","textAlign":"center","lock":{"move":false,"remove":false}} -->
-  <h2 class="has-text-align-center"></h2>
+  <!-- wp:heading {"level":2,"placeholder":"Heading"} -->
+  <h2></h2>
   <!-- /wp:heading -->
 
-  <!-- wp:group {"layout":{"type":"constrained"},"style":{"spacing":{"padding":{"top":"0.25rem","bottom":"0.25rem"}},"dimensions":{"minHeight":"2px"}},"lock":{"move":false,"remove":false}} -->
-  <div class="wp-block-group" style="min-height:2px">
-    <!-- wp:group {"style":{"dimensions":{"minHeight":"2px","width":"67%"},"color":{"background":"#3e173d"}},"layout":{"type":"constrained"},"lock":{"move":false,"remove":false}} -->
-    <div class="wp-block-group has-background" style="background-color:#3e173d;min-height:2px;width:67%">
-      <!-- wp:group {"style":{"dimensions":{"minHeight":"4px","width":"33%"},"color":{"background":"#6b2e6a"}},"layout":{"type":"constrained"},"lock":{"move":false,"remove":false}} -->
-      <div class="wp-block-group has-background" style="background-color:#6b2e6a;min-height:4px;width:33%"></div>
+  <!-- wp:group {"layout":{"type":"constrained"},"style":{"spacing":{"padding":{"top":"0.25rem","bottom":"0.25rem"}}},"lock":{"move":false,"remove":false}} -->
+  <div class="wp-block-group">
+    <!-- wp:group {"layout":{"type":"flex","justifyContent":"center"},"style":{"dimensions":{"height":"1rem"}},"lock":{"move":false,"remove":false}} -->
+    <div class="wp-block-group">
+      <!-- wp:group {"style":{"dimensions":{"height":"1rem","width":"67%"},"color":{"background":"#3e173d"}},"layout":{"type":"constrained"},"lock":{"move":false,"remove":false}} -->
+      <div class="wp-block-group has-background" style="background-color:#3e173d;height:1rem;width:67%">
+        <!-- wp:group {"style":{"dimensions":{"height":"2rem","width":"33%"},"color":{"background":"#6b2e6a"}},"layout":{"type":"flex","justifyContent":"center"},"lock":{"move":false,"remove":false}} -->
+        <div class="wp-block-group has-background" style="background-color:#6b2e6a;height:2rem;width:33%"></div>
+        <!-- /wp:group -->
+      </div>
       <!-- /wp:group -->
     </div>
     <!-- /wp:group -->
   </div>
   <!-- /wp:group -->
 
-  <!-- wp:paragraph {"fontSize":"sm","placeholder":"Subheading","textAlign":"center","lock":{"move":false,"remove":false}} -->
-  <p class="has-sm-font-size has-text-align-center"></p>
+  <!-- wp:paragraph {"fontSize":"md","placeholder":"Subheading"} -->
+  <p class="has-md-font-size"></p>
   <!-- /wp:paragraph -->
 </div>
 <!-- /wp:group -->
