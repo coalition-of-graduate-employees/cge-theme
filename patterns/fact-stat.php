37433eb2e4eface67b47d9211229d7b4b2292b87
Andrew Scherer <ascherer97@gmail.com>
2026-05-30 15:19:37 -0700

    fix syntax error in new pattern

--- patterns/fact-stat.php
+++ patterns/fact-stat.php
@@ -7,15 +7,15 @@
  */
 ?>
 <!-- wp:group {"layout":{"type":"constrained"},"style":{"spacing":{"blockGap":"0.75rem"}},"lock":{"move":false,"remove":false}} -->
-<div class="wp-block-group" data-lock="{"move":false,"remove":false}">
+<div class="wp-block-group">
   <!-- wp:heading {"level":2,"placeholder":"Heading","lock":{"move":false,"remove":false}} -->
-  <h2 data-lock="{"move":false,"remove":false}"></h2>
+  <h2></h2>
   <!-- /wp:heading -->
 
   <!-- wp:separator {"className":"is-style-wide","style":{"color":{"background":"#3e173d"}},"lock":{"move":false,"remove":false}} /-->
 
   <!-- wp:paragraph {"fontSize":"sm","placeholder":"Subheading","lock":{"move":false,"remove":false}} -->
-  <p class="has-sm-font-size" data-lock="{"move":false,"remove":false}"></p>
+  <p class="has-sm-font-size"></p>
   <!-- /wp:paragraph -->
 </div>
 <!-- /wp:group -->
